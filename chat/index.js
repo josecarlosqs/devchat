c246f807290e860e826475c16c1c689d4f14389f
Jose Carlos <josec.quichiz@gmail.com>
2017-04-16 11:42:58 -0500

    Fix: Deshabilitar backbutton una vez logueado

--- chat/index.js
+++ chat/index.js
@@ -1,6 +1,6 @@
 import React,{Component} from 'react'
 import {View,Text,StyleSheet,AppRegistry} from 'react-native'
-import {Scene, Router} from 'react-native-router-flux'
+import {Scene,Router,ActionConst} from 'react-native-router-flux'
 
 import Login from './screens/login'
 import Register from './screens/register'
@@ -12,9 +12,9 @@ export default class app extends Component {
     return (
     <Router>
       <Scene key="root">
-        <Scene key="login" component={Login} title="Login"/>
-        <Scene key="register" component={Register} title="Register"/>
-        <Scene key="chat" component={Chat}/>
+        <Scene key="login" component={Login} title="Login" />
+        <Scene key="register" component={Register} title="Register" />
+        <Scene key="chat" component={Chat} type={ActionConst.REPLACE} />
       </Scene>
     </Router>
     )
